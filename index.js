125444094fa41f26b9052627c0f5da8ef11066e3
Shawn Carpenter <shawn.carpeenter@gmail.com>
2020-08-11 15:24:15 -0700

    location working locally

--- index.js
+++ index.js
@@ -1,67 +1,9 @@
 require('dotenv').config();
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT ||3000
-app.use(cors());
-const  geoData  = require('./data/geo') 
-const weatherData = require('./data/weather')
+const { app } = require('./routes.js');
 
-app.get('/', (req, res) => {
-    try{ 
-        res.send(`<h1>This is an API endpoint</h1>
-            <p> To see location data use <a href="https://city-explorer-sjc.herokuapp.com/location">https://city-explorer-sjc.herokuapp.com/location</a></p>
-            <p> To see weather data use <a href="https://city-explorer-sjc.herokuapp.com/weather">https://city-explorer-sjc.herokuapp.com/weather</a></p>`);
-    } catch(e) {
-        res.status(500).json({ error: e.message });
-    }
-});
-
-function getLatLong(cityName){
-    //TODO: Replace hardcoded data with API call.
-    const city= geoData[0];
-    return {
-        formatted_query: city.display_name,
-        latitude: city.lat,
-        longitude: city.lon,
-    };
-}
-
-app.get('/location', (req, res) => {
-    const input = req.query.search;
-    try{
-        const formattedQuery = getLatLong(input);
-        res.json(formattedQuery);
-    } catch(e) {
-        res.status(500).json({ error: e.message });
-    }
-});
-
-function getWeather(lat, lon){
-    //TODO: replace hardcoded data with API call.
-    const data = weatherData.data;
-    return data.map(weatherItem => {
-        return {
-            forecast: weatherItem.weather.description,
-            time: new Date(weatherItem.ts * 1000),
-        }
-    });
-}
-
-app.get('/weather', (req, res) => {
-  try{
-     const userLat = req.query.latitude;
-     const userLon = req.query.longitude;
-     const mungedData = getWeather(userLat, userLon)
-     res.json(mungedData)
-
-  } catch(e) {
-    res.status(500).json({ error: e.message });
-  }
-  
-});
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-  
-})
+    console.log(`LISTEN SAYS: Example app listening at http://localhost:${port}`);
+});
+
+
---
+++ routes.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const cors = require('cors');
+const request = require('superagent');
+const app = express();
+
+app.use(cors());
+
+app.use(express.static('public'));
+
+const {
+    GEOCODE_API_KEY,
+    WEATHER_API_KEY,
+    TRAIL_API_KEY
+} = process.env;
+
+
+app.get('/', (req, res) => {
+    try{ 
+        res.send(`<h1>This is an API endpoint</h1>
+            <p> To see location data use <a href="https://city-explorer-sjc.herokuapp.com/location">https://city-explorer-sjc.herokuapp.com/location</a></p>
+            <p> To see weather data use <a href="https://city-explorer-sjc.herokuapp.com/weather">https://city-explorer-sjc.herokuapp.com/weather</a></p>`);
+    } catch(e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+
+async function getLatLong(cityName){
+
+    const response= await request.get(`https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${cityName}&format=json`);
+
+    const city = response.body[0]
+
+    return {
+        formatted_query: city.display_name,
+        latitude: city.lat,
+        longitude: city.lon,
+    };
+}
+
+app.get('/location', async (req, res) => {
+    try{
+        const userInput = req.query.search;
+        const formattedQuery = await getLatLong(userInput);
+        res.json(formattedQuery);
+    } catch(e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+
+function getWeather(lat, lon){
+    //TODO: replace hardcoded data with API call.
+    const data = weatherData.data;
+    return data.map(weatherItem => {
+        return {
+            forecast: weatherItem.weather.description,
+            time: new Date(weatherItem.ts * 1000),
+        }
+    });
+}
+
+app.get('/weather', (req, res) => {
+  try{
+     const userLat = req.query.latitude;
+     const userLon = req.query.longitude;
+     const mungedData = getWeather(userLat, userLon)
+     res.json(mungedData)
+
+  } catch(e) {
+    res.status(500).json({ error: e.message });
+  }
+  
+});
+
+module.exports = {
+    app
+};
