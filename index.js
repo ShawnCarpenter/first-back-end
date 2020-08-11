1ce7df274efe688209aff2b2932762c7bc1f0833
Shawn Carpenter <shawn.carpeenter@gmail.com>
2020-08-11 10:36:00 -0700

    added .env file and start script

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.PORT ||3000
 app.use(cors());
 const  geoData  = require('./data/geo') 
 const weatherData = require('./data/weather')
